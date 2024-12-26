a9b4262383a688749a75ec5bba443cd4dd5be20f
TakHoya <130663193+Hoya0718@users.noreply.github.com>
2024-12-26 23:37:51 +0900

    도메인형식 구조 2024-12-26 23:37

--- src/main/java/org/boot/projectboard/index/Controller/IndexController.java
+++ src/main/java/org/boot/projectboard/index/Controller/IndexController.java
@@ -1,13 +1,14 @@
-package domain.Controller;
+package org.boot.projectboard.index.Controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @Controller
 public class IndexController {
+
     @GetMapping("/")
     public String index() {
-        return "index.html";
+        return "redirect:/index.html";
     }
+
 }
