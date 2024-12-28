b3cf5c572bc7a5e067910ae947129279ad9cfedd
TakHoya <130663193+Hoya0718@users.noreply.github.com>
2024-12-29 00:15:53 +0900

    기본 경로 이동 2024-12-29 00:15

--- src/main/java/org/boot/projectboard/board/Controller/BoardController.java
+++ src/main/java/org/boot/projectboard/board/Controller/BoardController.java
@@ -1,12 +1,18 @@
 package org.boot.projectboard.board.Controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class BoardController {
-    @GetMapping("board")
-    public String board() {
-        return "board";
+    @GetMapping("/layout/board/")
+    public String board(@RequestParam(value = "post", required = false)String post, Model model) {
+        if(post != null){
+            model.addAttribute("post", post);
+            return "layout/post";
+        }
+        return "layout/board";
     }
 }
