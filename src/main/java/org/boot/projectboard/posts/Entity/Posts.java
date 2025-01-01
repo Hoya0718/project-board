c4f282380201b65dd6d28e0559dd6bb827a2cc70
TakHoya <130663193+Hoya0718@users.noreply.github.com>
2025-01-01 17:09:44 +0900

    edit 라이브러리 추가 2025-01-01 17:10

--- src/main/java/org/boot/projectboard/posts/Entity/Posts.java
+++ src/main/java/org/boot/projectboard/posts/Entity/Posts.java
@@ -15,6 +15,7 @@ public class Posts {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
+    @Column(nullable = false)
     private String title;
 
     private String content;
